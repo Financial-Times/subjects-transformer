a95c7ee1f05833a4613d095ac31fc8da1d4a4368
Martin Garton <martin.garton@ft.com>
2016-02-12 11:26:51 +0000

    remove custom uuid code

--- transformer.go
+++ transformer.go
@@ -1,11 +1,15 @@
 package main
 
+import (
+	"github.com/pborman/uuid"
+)
+
 type subjectTransformer struct {
 }
 
 func (tr *subjectTransformer) transform(t term) subject {
 	return subject{
-		UUID:          NewNameUUIDFromBytes([]byte(t.ID)).String(),
+		UUID:          uuid.NewMD5(uuid.UUID{}, []byte(t.ID)).String(),
 		CanonicalName: t.CanonicalName,
 		TmeIdentifier: t.ID,
 		Type:          "Subject",
